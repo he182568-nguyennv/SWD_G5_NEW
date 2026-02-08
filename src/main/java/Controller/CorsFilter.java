9c2b61188e2c41fa34a5a5758cfa00b2d7aeb3bc
he182568-nguyennv <nguyenheilwig@gmail.com>
2026-02-08 10:11:56 +0700

    config header vite

--- src/main/java/Controller/CorsFilter.java
+++ src/main/java/Controller/CorsFilter.java
@@ -16,7 +16,7 @@ public class CorsFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) request;
 
         // 1. Cấu hình CORS (Dùng chung cho cả dự án)
-        resp.setHeader("Access-Control-Allow-Origin", "http://localhost:3000"); // Cho phép React
+        resp.setHeader("Access-Control-Allow-Origin", "http://localhost:5173"); // Cho phép Vite
         resp.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         resp.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
         resp.setHeader("Access-Control-Allow-Credentials", "true");
